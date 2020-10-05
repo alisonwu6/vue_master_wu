84cfc72807d26de4a28826ad65431dcc4c5cb13d
Alison Wu <alison.wu23@gmail.com>
2020-10-05 08:47:10 +0800

    [modify] remove useless code

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,8 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    // HelloWorld
-  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HelloWorld from '../components/HelloWorld.vue'
-
 Vue.use(VueRouter)
 
 const routes = [
@@ -10,14 +8,6 @@ const routes = [
     name: 'HelloWorld',
     component: HelloWorld
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
