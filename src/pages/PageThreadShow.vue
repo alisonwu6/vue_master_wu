d662e82061f1fe29f3b3dc5db2f1d551af710f41
Alison Wu <alison.wu23@gmail.com>
2020-10-06 11:11:32 +0800

    [add] 拆組件

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,23 +2,18 @@
     <div class="col-large push-top">
         <h1>{{thread.title}}</h1>
         <PostList :posts="posts" />
-        <form @submit.prevent="addPost">
-            <div class="form-group">
-                <textarea name="" id="" cols="30" rows="10" class="form-input" v-model="newPostText"></textarea>
-            </div>
-            <div class="form-actions">
-                <button class="btn-blue">Submit post</button>
-            </div>
-        </form>
+        <PostEditor @save="addPost" :threadId='id'/>
     </div>
 </template>
 
 <script>
     import sourceData from '@/data'
     import PostList from '@/components/PostList'
+    import PostEditor from '@/components/PostEditor';
     export default {
         components: {
-            PostList
+            PostList,
+            PostEditor,
         },
         props: {
             id: {
@@ -29,7 +24,6 @@
         data() {
             return {
                 thread: sourceData.threads[this.id],
-                newPostText: ''
             }
         },
         computed: {
@@ -37,23 +31,16 @@
                 const postIds = Object.values(this.thread.posts)
                 return Object.values(sourceData.posts)
                     .filter(post => postIds.includes(post['.key']))
-            }
+            },
         },
         methods: {
-            addPost() {
-                const postId = 'greatPost' + Math.random()
-                const post = {
-                    text: this.newPostText,
-                    publishedAt: Math.floor(Date.now() / 1000),
-                    threadId: this.id,
-                    userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
-                    '.key': postId
-                }
+            addPost (eventData) {
+                const post = eventData.post
+                const postId = eventData.post['.key']
                 this.$set(sourceData.posts, postId, post)
                 this.$set(this.thread.posts, postId, postId)
                 this.$set(sourceData.users[post.userId].posts, postId, postId)
-                this.newPostText = ''
             }
-        }
+        },
     }
 </script>
