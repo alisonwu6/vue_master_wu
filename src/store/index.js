5ee55ef2fe1458a77600ecd2ab756881f3f93bf9
Alison Wu <alison.wu23@gmail.com>
2020-10-14 10:47:55 +0800

    [add] redirect on publish

--- src/store/index.js
+++ src/store/index.js
@@ -27,17 +27,20 @@ export default new Vuex.Store({
         },
 
         createThread ({state, commit, dispatch}, {text, title, forumId}) {
-            const threadId = 'greatThread' + Math.random()
-            const userId = state.authId
-            const publishedAt = Math.floor(Date.now() / 1000)
-
-            const thread = {'.key': threadId, title, forumId, publishedAt, userId}
-
-            commit('setThread', {threadId, thread})
-            commit('appendThreadToForum', {forumId, threadId})
-            commit('appendThreadToUser', {userId, threadId})
-
-            dispatch('createPost', {text, threadId})
+            return new Promise((resolve) => {
+                const threadId = 'greatThread' + Math.random()
+                const userId = state.authId
+                const publishedAt = Math.floor(Date.now() / 1000)
+    
+                const thread = {'.key': threadId, title, forumId, publishedAt, userId}
+    
+                commit('setThread', {threadId, thread})
+                commit('appendThreadToForum', {forumId, threadId})
+                commit('appendThreadToUser', {userId, threadId})
+    
+                dispatch('createPost', {text, threadId})
+                resolve(state.threads[threadId])
+            })
         },
 
         updateUser ({commit}, user) {
